93d49d342f4a8b0a6d771a4f236c34326e0f0db3
myiq_032 <n6ghurl07@gmail.com>
2019-11-14 14:39:40 +0900

    file process for grayscale

--- PythonApplication_imgprocessing.py
+++ PythonApplication_imgprocessing.py
@@ -1,23 +1,32 @@
-import numpy as np
+import os
 import cv2
-if __name__ == '__main__':
+import glob
 
-    #画像の読み込み
-    img1 = cv2.imread(r"C:\Users\myiq_\downloads\background_ori.jpg", 1)
-    img2 = cv2.imread(r"C:\users\myiq_\downloads\background_add.jpg", 1)
+#グレースケール関数
+def gray_scale(path):
+    img = cv2.imread(path, 1) #カラー画像の読み込み
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #グレースケール変換
+    return gray
 
-    fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+#指定されたディレクトリ内のファイル情報を取得する関数
+def bundle(dir, outdir):
+    path_list = glob.glob(dir + '\*') #指定されたディレクトリ内のすべてのファイルを取得
+    name_list = [] #ファイル名の空リストを定義
+    ext_list = [] #拡張子の空リストを定義
 
-    fgmask = fgbg.apply(img1)
-    fgmask = fgbg.apply(img2)
+    #ファイルのフルパスからファイル名と拡張子を抽出
+    for i in path_list:
+        file = os.path.basename(i) #拡張子ありのファイル名を取得
+        name, ext = os.path.splitext(file) #拡張子なしのファイル名と拡張子を取得
+        name_list.append(name) #拡張子なしのファイル名をリスト化
+        ext_list.append(ext) #拡張子をリスト化
 
+        out_path = os.path.join(*[outdir, name + '_gray' + ext]) #保存パスを作成
 
-    #表示
-    cv2.imshow('frame', fgmask)
+        img = gray_scale(i) #グレイスケール関数を実行
+        cv2.imwrite(out_path, img) #グレイスケール後の画像を保存
 
-    #検出画像
-    bg_diff_path = r'C:\Users\myiq_\downloads\diff.jpg'
-    cv2.imwrite(bg_diff_path, fgmask)
+    return
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#ファイルを探してリサイズする関数を実行
+bundle('C:\\sample_img', 'C:\\sample_grayimg')
